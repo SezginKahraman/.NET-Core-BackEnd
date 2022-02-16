8dcd870df03db10b5840fd0edb7a3a6ba3be401a
SezginKahraman <sezgin.kahraman@gmail.com>
2022-02-16 17:28:56 +0300

    ValidationRules

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -1,13 +1,17 @@
 ﻿using Business.Abstract;
 using Business.Constants;
+using Business.ValidationRules.FluentValidation;
+using Core.CrossCuttingConcerns.Validation;
 using Core.Utilities.Results;
 using DataAccess.Abstract;
 using DataAccess.Concrete.InMemory;
 using Entities.Concrete;
 using Entities.DTOs;
+using FluentValidation;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using Core.Aspects.Autofac.Validation;
 
 namespace Business.Concrete
 {
@@ -20,16 +24,34 @@ namespace Business.Concrete
             _productDal = productDal;
         }
 
+        [ValidationAspect(typeof(ProductValidator))]
         public IResult Add(Product product)
         {
-            if (product.ProductName.Length<2)
-            {
-                return new ErrorResult(Messages.ProductNameInvalid);
-            }
-            
+
+            //validation
+
+            //bu alttaki kodlar Fluentvaldation ile yapılır
+            //if (product.UnitPrice<=0)
+            //{
+            //    return new ErrorResult(Messages.UnitPriceInvalid);
+            //}
+            //if (product.ProductName.Length<2)
+            //{
+            //    return new ErrorResult(Messages.ProductNameInvalid);
+            //}
+            //var context = new ValidationContext<Product>(product);
+            //ProductValidator productValidator = new ProductValidator();
+            //var result = productValidator.Validate(context);
+            //if (!result.IsValid)
+            //{
+            //    throw new ValidationException(result.Errors);
+            //}
+            //ValidationTool.Validate(new ProductValidator(), product);
+
             _productDal.Add(product);
 
             return new SuccessResult(Messages.ProductAdded);
+            //üstteki kodları zaten ValidationRules klasöründe yazdık !
         }
 
 
---
+++ Core/CrossCuttingConcerns/Validation/ValidationTool.cs
@@ -0,0 +1,24 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.CrossCuttingConcerns.Validation
+{
+    public static class ValidationTool
+    {
+
+        public static void Validate(IValidator validator, object entity)
+        {
+            var context = new ValidationContext<object>(entity);
+            //ProductValidator productValidator = new ProductValidator(); (IValidator validator
+            //yazınca ihtiyaç kalmadı
+            var result = validator.Validate(context);
+            if (!result.IsValid)
+            {
+                throw new ValidationException(result.Errors);
+            }
+        }
+
+    }
+}
