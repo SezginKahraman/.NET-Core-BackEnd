5f4c0f4b3c842a099922800eb6645f95df213a37
SezginKahraman <sezgin.kahraman@gmail.com>
2022-02-17 18:41:58 +0300

    JWT-2

---
+++ Core/Utilities/Security/Encryption/SigningCredentialsHelper.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Utilities.Security.Encryption
+{
+    public class SigningCredentialsHelper
+    {
+        public static SigningCredentials CreateSigningCredentials(SecurityKey securityKey)
+        {
+            return new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha512Signature);
+
+            //14.gün 1.42.10
+
+        }
+
+    }
+}
